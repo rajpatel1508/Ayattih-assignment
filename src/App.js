f19ddcc71f2497dc54bf29ba05790e604ffa9a66
unknown <gameman777.rp@gmail.com>
2023-02-16 19:08:56 +0530

    algoritm changed

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { Navbar, Container, Alert, Button } from 'react-bootstrap';
 import CloseIcon from '@mui/icons-material/Close';
 import DoneIcon from '@mui/icons-material/Done';
+import './App.css'
 
 function App() {
   const [problemNumber, setproblemNumber] = useState(0);
-  const problems = [[[2, 5], [4, 7], [6, 9], [8, 11]], [[2, 5], [4, 8], [6, 11], [8, 14]], [[2, 1], [5, 3], [8, 4], [11, 7]], [[4, 1], [5, 2], [7, 4], [8, 7]]];
+  const problems = [[[2, 5], [4, 7], [6, 9], [8, 11]], [[2, 5], [4, 8], [6, 11], [8, 14]], [[2, 1], [5, 3], [8, 4], [11, 6]], [[4, 1], [5, 2], [7, 4], [8, 7]]];
   const [answer1, setAnswer1] = useState('');
   const [answer2, setAnswer2] = useState('');
   const [showAlert, setShowAlert] = useState(false);
@@ -19,13 +20,21 @@ function App() {
   const handleSubmit = () => {
     const userAnswer = [parseInt(answer1), parseInt(answer2)];
 
+    // calculations for change in first number of pair
     const d1 = problems[problemNumber][1][0] - problems[problemNumber][0][0];
-    const d2 = problems[problemNumber][1][1] - problems[problemNumber][0][1];
-    const d3 = problems[problemNumber][2][1] - problems[problemNumber][1][1];
-    const d4 = problems[problemNumber][3][1] - problems[problemNumber][2][1];
-    const d = d4 - d2;
-    const nextinc = d3 + d;
-    const expectedAnswer = [problems[problemNumber][3][0] + d1, problems[problemNumber][3][1] + nextinc];
+    const d2 = problems[problemNumber][2][0] - problems[problemNumber][1][0];
+    const d3 = problems[problemNumber][3][0] - problems[problemNumber][2][0];
+    const diff1 = d3 - d1;
+    const nextinc1 = d2 + diff1;
+
+    // calculations for change in second number of pair
+    const d4 = problems[problemNumber][1][1] - problems[problemNumber][0][1];
+    const d5 = problems[problemNumber][2][1] - problems[problemNumber][1][1];
+    const d6 = problems[problemNumber][3][1] - problems[problemNumber][2][1];
+    const diff2 = d6 - d4;
+    const nextinc2 = d5 + diff2;
+
+    const expectedAnswer = [problems[problemNumber][3][0] + nextinc1, problems[problemNumber][3][1] + nextinc2];
     const isCorrect = userAnswer[0] === expectedAnswer[0] && userAnswer[1] === expectedAnswer[1];
 
     setAlertVariant(isCorrect ? 'success' : 'danger');
@@ -50,16 +59,16 @@ function App() {
       </Navbar>
       <div className="bg-grey">
         <Container>
-          <p className="mt-4 mb-4" style={{ backgroundColor: "white", padding: "10px", borderRadius: '5px' }}> Mental Maths - Level 1</p>
+          <p className="heading mt-4 mb-4"> Mental Maths - Level 1</p>
           <div style={{ textAlign:"center" }}>
             <h1>{problems[problemNumber].map((problem, index) => {
               return `(${problem[0]},${problem[1]})`
-            })}(<input type="text" style={{ width: "60px", border: "none" }} value={answer1} onChange={(e) => setAnswer1(e.target.value)} />,<input type="text" style={{ width: "60px", border: "none" }} value={answer2} onChange={(e) => setAnswer2(e.target.value)} />)</h1>
+            })}(<input className='AnswerInput' type="text" value={answer1} onChange={(e) => setAnswer1(e.target.value)} />,<input className='AnswerInput' type="text" value={answer2} onChange={(e) => setAnswer2(e.target.value)} />)</h1>
             <div style={{margin:"50px"}}>
               <Button variant="dark" onClick={handleSubmit}>Submit</Button>
             </div>
           </div>
-          {showAlert && <Alert style={{ backgroundColor: alertVariant === 'danger' ? "red" : "green", color: 'white', position: 'relative' }} className="mt-4">{alertVariant === 'danger' ? <CloseIcon/> : <DoneIcon/>}{alertMessage}{showAlert && problemNumber < 3 && <Button style={{ right: "0", position: "absolute", top: '50%', transform: "translateY(-50%)", marginRight: '20px' }} variant="light" onClick={handleNextProblem}>Next</Button>}</Alert>}
+          {showAlert && <Alert style={{ backgroundColor: alertVariant === 'danger' ? "red" : "green", color: 'white', position: 'relative' }} className="mt-4">{alertVariant === 'danger' ? <CloseIcon/> : <DoneIcon/>}{alertMessage}{showAlert && problemNumber < 3 && <Button className='nextButton' variant="light" onClick={handleNextProblem}>Next</Button>}</Alert>}
         </Container>
       </div>
     </div>
